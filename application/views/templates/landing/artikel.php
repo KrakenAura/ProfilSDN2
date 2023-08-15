174feb715c1f6187be561c9c281693af792f4402
KrakenAura <akarim18641@gmail.com>
2023-08-15 21:31:30 +0700

    fix artikel

--- application/views/templates/landing/artikel.php
+++ application/views/templates/landing/artikel.php
@@ -1,21 +1,22 @@
 <header class="masthead" style="background-image: url('<?= base_url('/assets/Home/') ?>img/about-bg.jpg')">
-            <div class="container position-relative px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <div class="page-heading">
-                            <h1>About Us</h1>
-                            <span class="subheading">Halo</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!DOCTYPE html>
+  <div class="container position-relative px-4 px-lg-5">
+    <div class="row gx-4 gx-lg-5 justify-content-center">
+      <div class="col-md-10 col-lg-8 col-xl-7">
+        <div class="page-heading">
+          <h1>About Us</h1>
+          <span class="subheading">Halo</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<!DOCTYPE html>
 <html lang="en">
-  <style>
+<style>
 
 
-  </style>
+</style>
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,13 +24,14 @@
   <link rel="stylesheet" href="styles.css">
   <title>Artikel SDN 2 Mulyoagung Malang</title>
 </head>
+
 <body>
   <div class="container mt-5">
     <div class="row">
       <div class="col-lg-8">
         <article>
           <h1 class="mb-3">Eksplorasi Kreativitas dan Pembelajaran di SDN 2 Mulyoagung Malang</h1>
-          
+
         </article>
       </div>
       <div class="col-lg-4">
@@ -78,6 +80,7 @@
 </body>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -93,14 +96,17 @@
       border: 1px solid #e0e0e0;
       border-radius: 5px;
     }
+
     .article h2 {
       color: #333;
     }
+
     .article p {
       color: #666;
     }
   </style>
 </head>
+
 <body>
   <div class="container mt-5">
     <div class="row">
@@ -128,11 +134,11 @@
           <p>Tips untuk membantu siswa mengatasi rasa gugup sebelum ujian, serta informasi penting tentang jadwal ulangan dan libur sekolah.</p>
         </div>
       </div>
-    </div> class="col-md-6">
-    <div class="article">
-      <h2> cerita inspiratif</h2>
-      <p> ketika aku merindukanmu aku hanya bisa terdiam dan merenung ? apa kabar kamu disana semoga kita baik baik saja ya <p>
-  </div>
+      <div class="article">
+        <h2> cerita inspiratif</h2>
+        <p> ketika aku merindukanmu aku hanya bisa terdiam dan merenung ? apa kabar kamu disana semoga kita baik baik saja ya
+        <p>
+      </div>
 </body>
 
 </html>
