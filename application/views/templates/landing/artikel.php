5a30f21b04073acb1bff746513e9261dbe1beb1b
KrakenAura <akarim18641@gmail.com>
2023-08-22 13:25:44 +0700

    Remake Article

--- application/views/templates/landing/artikel.php
+++ application/views/templates/landing/artikel.php
@@ -1,113 +1,12 @@
 <header class="masthead" style="background-image: url('<?= base_url('/assets/Home/') ?>img/about-bg.jpg')">
-  <div class="container position-relative px-4 px-lg-5">
-    <div class="row gx-4 gx-lg-5 justify-content-center">
-      <div class="col-md-10 col-lg-8 col-xl-7">
-        <div class="page-heading">
-          <h1>About Us</h1>
-          <span class="subheading">Halo</span>
+    <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <div class="page-heading">
+                    <h1>Artikel</h1>
+                    <span class="subheading">Berita dan Informasi SD Negeri 2 Mulyoagung</span>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </header>
-<!DOCTYPE html>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="styles.css">
-  <title>Web Sekolah SD</title>
-  <style>
-    /* Gaya untuk bagian artikel */
-    .article {
-      margin-bottom: 30px;
-      padding: 20px;
-      background-color: #f7f7f7;
-      border: 1px solid #e0e0e0;
-      border-radius: 5px;
-    }
-
-    .article h2 {
-      color: #333;
-    }
-
-    .article p {
-      color: #666;
-    }
-  </style>
-</head>
-
-<body>
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-8">
-        <article class="article">
-          <h1 class="mb-3">Eksplorasi Kreativitas dan Pembelajaran di SDN 2 Mulyoagung Malang</h1>
-          <p>Isi artikel yang menarik dan informatif.</p>
-        </article>
-      </div>
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Eksplorasi Pembelajaran SDN 2 Mulyoagung</h5>
-            <ul class="list-unstyled">
-              <li><a href="<?= base_url('/index.php/ArtikelController/artikel') ?>">Berita</a></li>
-              <li><a href="<?= base_url('/index.php/ArtikelController/artikel') ?>">Informasi</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <!DOCTYPE html>
-      <html lang="en">
-
-      <head>
-        <!-- Meta tags, links to stylesheets, title, and inline styles -->
-      </head>
-
-      <body>
-        <div class="container mt-5">
-          <div class="row">
-            <div class="col-md-6">
-              <article class="article">
-                <h2>Berita</h2>
-                <p>Informasi tentang acara lomba menulis yang berhasil dimenangkan oleh siswa-siswi sekolah kami. Selamat kepada para pemenang! fjhwejviojopkocpojvojpokoasopokfoisajvasjvnjnvnaoijikasvjijaijvia</p>
-              </article>
-            </div>
-
-            <div class="col-md-6">
-              <article class="article">
-                <h2>Informasi</h2>
-                <p>Tips untuk membantu siswa mengatasi rasa gugup sebelum ujian serta informasi penting tentang jadwal ulangan dan libur sekolah halo semuanya uefbjijnenfuemfojiflinfkeknkemfkoiqngokqngnqoijgo</p>
-              </article>
-            </div>
-          </div>
-        </div>
-      </body>
-
-      </html>
-
-      <Style>
-        .article {
-          height: 100%;
-          /* Set tinggi artikel agar sama */
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-        }
-
-        .article h2 {
-          color: #333;
-          margin-bottom: 10px;
-        }
-
-        .article p {
-          color: #666;
-        }
-      </Style>
-
-
-</html>
