cc12f7efb50c30951ab0010e2ef61384a54d0892
KrakenAura <akarim18641@gmail.com>
2023-09-05 16:15:29 +0700

    delete comment

--- application/views/templates/landing/struktur_organisasi.php
+++ application/views/templates/landing/struktur_organisasi.php
@@ -11,28 +11,6 @@
         </div>
     </header>
 
-    <!-- <img src="<?= base_url('/assets/Resource/sambutan/avatar.jpg') ?>" class="card-img-top" alt="..."> -->
-    <!-- Struktur Organisasi Cards-->
-    <!-- <div class="card-group">
-        <div class="card">
-            <div class="card-body">
-                <img src="<?= base_url('/assets/Resource/sambutan/avatar.jpg') ?>" class="card-img-top" alt="...">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-        <div class="card" style="width: 18rem;">
-            <img src="<?= base_url('/assets/Resource/sambutan/avatar.jpg') ?>" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </div> -->
     <style>
         .card {
             border-radius: 20px;
